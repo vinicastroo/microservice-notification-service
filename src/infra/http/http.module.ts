6d5036e9bb23e8d4ae83fae1603db28d5824c4ad
Vinicius Castro <viniciusc.d.c@hotmail.com>
2023-01-29 22:59:38 -0300

    feat/microservice notification service:finished

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,23 @@
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { Module } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification';
+import { DataBaseModule } from '../database/database.module';
+import { NotificationsController } from './controller/notifications.controller';
+
+@Module({
+  imports: [DataBaseModule],
+  controllers: [NotificationsController],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
+})
+export class HttpModule {}
